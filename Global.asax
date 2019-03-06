9aae49f5cb4310c058815066edfc57a7291b44c6
junhpark <junhpark@taihoinst.com>
2019-03-06 17:00:26 +0900

    INIT COMMIT

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HanjinChatBot.WebApiApplication" Language="C#" %>
---
+++ Models/RootObject.cs
@@ -0,0 +1,13 @@
+﻿namespace HanjinChatBot.Models
+{
+	using Newtonsoft.Json;
+	using System;
+	using System.Collections.Generic;
+
+	[Serializable]
+	public class RootObject
+	{
+		public List<Search> items { get; set; }
+		public int display { get; set; }
+	}
+}
