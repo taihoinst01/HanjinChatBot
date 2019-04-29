137d2311a17c6ee79a4500144ccc6f46dfdb13fe
junhpark <junhpark@taihoinst.com>
2019-04-29 15:38:53 +0900

    수정

--- App_Start/Controllers/MessagesController.cs
+++ App_Start/Controllers/MessagesController.cs
@@ -208,7 +208,7 @@ namespace HanjinChatBot
                     {
                         foreach (CardList tempcard in dialogs.dialogCard)
                         {
-                            tempAttachment = dbutil.getAttachmentFromDialog(tempcard, activity, "INIT");
+                            tempAttachment = dbutil.getAttachmentFromDialog(tempcard, activity);
                             initReply.Attachments.Add(tempAttachment);
 
                             //2018-11-26:KSO:INIT Carousel 만드는부분 추가
@@ -557,14 +557,12 @@ namespace HanjinChatBot
                                 Activity commonReply = activity.CreateReply();
                                 Attachment tempAttachment = new Attachment();
                                 DButil.HistoryLog("dlg.dlgType : " + dlg.dlgType);
-
-                                string userSSO = "NONE";
                                 
                                 if (dlg.dlgType.Equals(CARDDLG))
                                 {
                                     foreach (CardList tempcard in dlg.dialogCard)
                                     {
-                                        tempAttachment = dbutil.getAttachmentFromDialog(tempcard, activity, userSSO);
+                                        tempAttachment = dbutil.getAttachmentFromDialog(tempcard, activity);
 
                                         if (tempAttachment != null)
                                         {
