f190dbcaa1246ddb02da7a888b27a319cf5035fa
junhpark <junhpark@taihoinst.com>
2019-09-02 09:31:05 +0900

    0902 수정1

--- App_Start/Controllers/MessagesController.cs
+++ App_Start/Controllers/MessagesController.cs
@@ -1645,9 +1645,9 @@ namespace HanjinChatBot
                             authName = uData[0].userName;//모바일 인증 체크(이름)
                             authNumber = uData[0].authNumber;//모바일 인증 체크(인증번호)
 
-                            //mobilePC = "MOBILE";//TEST 용 반드시 지울 것!!!!(에뮬레이터용)
+                            mobilePC = "MOBILE";//TEST 용 반드시 지울 것!!!!(에뮬레이터용)
                             //mobilePC = "PC";//TEST 용 반드시 지울 것!!!!(에뮬레이터용)
-                            //requestPhone = "01022840610";//TEST 용 반드시 지울 것!!!!김은영대리
+                            requestPhone = "01022840610";//TEST 용 반드시 지울 것!!!!김은영대리
                             //requestPhone = "01075013741";//TEST 용 반드시 지울 것!!!!이채원강사
                             //requestPhone = "01027185020";//TEST 용 반드시 지울 것!!!!개발테스트용
                             //db.UserCheckUpdate(activity.ChannelId, activity.Conversation.Id, "USER_PHONE", requestPhone);//TEST 용 반드시 지울 것!!!!
@@ -7229,7 +7229,7 @@ namespace HanjinChatBot
                                                 {
                                                     foreach (JObject jobj4 in obj4)
                                                     {
-                                                        if (jobj4["ret_cod"].ToString().Equals("9041"))
+                                                        if (jobj4["ret_cod"].ToString().Equals("9061"))
                                                         {
                                                             totalPage = 1;
                                                             plCard = new UserHeroCard()
@@ -7240,7 +7240,7 @@ namespace HanjinChatBot
                                                             plAttachment = plCard.ToAttachment();
                                                             apiMakerReply.Attachments.Add(plAttachment);
                                                         }
-                                                        else if (jobj4["ret_cod"].ToString().Equals("9042"))
+                                                        else if (jobj4["ret_cod"].ToString().Equals("9062"))
                                                         {
                                                             totalPage = 1;
                                                             plCard = new UserHeroCard()
